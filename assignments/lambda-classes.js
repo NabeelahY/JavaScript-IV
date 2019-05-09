4cf23c3a7475784b3cdabaa114b006f367d50e70
Nabeelah <nabeelahyousuph@gmail.com>
2019-05-09 13:59:28 +0100

    Create all class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -25,3 +25,36 @@ class Instructor extends Person {
         console.log(`${student.name} receives a perfect score on ${subject}`);
     }
 }
+
+class Student extends Person {
+    constructor(student) {
+        super(student);
+        this.previousBackground = student.previousBackground,
+        this.className = student.className,
+        this.favSubjects = student.favSubjects
+    }
+    listsSubjects() {
+       this.favSubjects.forEach(subject => console.log(subject)); 
+    }
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`)
+    }
+    sprintChallenge () {
+        console.log(`${student.name} has begun sprint challenge on ${subject}`)
+    }
+}
+
+class ProjectManagers {
+    constructor(pm) {
+        super(pm);
+        this.gradClassName = pm.gradClassName,
+        this.favInstructor = pm.favInstructor
+    }
+    standUp(channel) {
+        console.log(`${this.name} announces to ${channel}, @channel standy times!​​​​​`)
+    }
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)
+    }
+}
+
